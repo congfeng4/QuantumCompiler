f6fa7ea3605a6042aaace25686f719c1f6c74b82
huawei-od-man <2120400017@stu.hrbust.edu.cn>
2025-07-20 23:05:46 +0800

    增大了Batch size之后，8条轨迹也能收敛了 bs=128 翻倍 256，T=8 轨迹数量，都是SABRE初始化，不同的电路

--- contrib/pretrain_env.py
+++ contrib/pretrain_env.py
@@ -352,19 +352,29 @@ if __name__ == '__main__':
                                     prefix='20Q_gate_Tokyo_val')
 
     circuit_list = list(Path('../data/20Q_gate_Tokyo/circuits').glob('*.qasm'))
-    qc = QuantumCircuit.from_qasm_file(str(circuit_list[0]))
-    T = 5
+    T = 1
 
-    for j in range(T):
-        init = get_initial_mapping(qc, hardware, InitialMappingStrategy.RANDOM)
-        ha_mapping(
-            # 2 for train, 1 for val.
-            collector=collector_train if j != T - 1 else collector_val,
-            quantum_circuit=qc,
-            initial_mapping=init,
-            hardware=hardware,
-            strategy='best',
-        )
+    for i in range(8):
+        qc = QuantumCircuit.from_qasm_file(str(circuit_list[i]))
+        for j in range(T):
+            init = get_initial_mapping(qc, hardware, InitialMappingStrategy.SABRE)
+            ha_mapping(
+                collector=collector_train,
+                quantum_circuit=qc,
+                initial_mapping=init,
+                hardware=hardware,
+                strategy='best',
+            )
+
+    qc = QuantumCircuit.from_qasm_file(str(circuit_list[-1]))
+    init = get_initial_mapping(qc, hardware, InitialMappingStrategy.SABRE)
+    ha_mapping(
+        collector=collector_val,
+        quantum_circuit=qc,
+        initial_mapping=init,
+        hardware=hardware,
+        strategy='best',
+    )
 
     collector_train.save()
     collector_val.save()
--- script/behave_clone_pretrain.py
+++ script/behave_clone_pretrain.py
@@ -21,7 +21,7 @@ from imitation.algorithms.bc import BehaviorCloningLossCalculator
 
 
 def main():
-    bs = 128
+    bs = 256
     log_dir = f"../log/pretrain/exe-swap"
     shutil.rmtree(log_dir, ignore_errors=True)
 
