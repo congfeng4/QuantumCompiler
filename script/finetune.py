42a432125ef6ecd2eabeb61b911719213b61b0aa
huawei-od-man <2120400017@stu.hrbust.edu.cn>
2025-08-20 00:33:52 +0800

    final_reward=0, when rew approaches 0, the gradient becomes unstable. Because value of rew is too small?

--- script/finetune.py
+++ script/finetune.py
@@ -8,16 +8,17 @@ def get_param_space(rs_weights: list[float], final_rewards: list[int], horizon_l
 
 
 if __name__ == '__main__':
-    mode = 'transformer'
+    mode = 'gru'
+    seqlen = 0.2
 
     for path in get_short_20Q_circuits(min_gatelen=0, max_gatelen=9999):
     # path = '../data/20Q_gate_Tokyo/circuits/20Q_gate_Tokyo_large_1_5_1.5_no.3.qasm'
         run_maskable_ppo(
             hardware='Tokyo',
             circuit_path=path,
-            seqlen=9999,
-            final_reward=0,
-            output_dirname=f'20Q_gate_seqlen=all_mode={mode}',
+            seqlen=seqlen,
+            final_reward=1000,
+            output_dirname=f'20Q_gate_seqlen={seqlen}_mode={mode}',
             reward_shaping_weight=10,
             mode=mode,
             save_result=True,
