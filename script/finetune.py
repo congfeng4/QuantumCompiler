07e6f4df5ca12db2164337f6a572bccc4506a927
huawei-od-man <2120400017@stu.hrbust.edu.cn>
2025-08-23 19:12:33 +0800

    Setup sample gatelen run.

--- script/finetune.py
+++ script/finetune.py
@@ -1,32 +1,38 @@
 from contrib.common import Unit, get_cnot_num
 from contrib.maskable_ppo import run_maskable_ppo, CircuitDataset
 
+
 if __name__ == '__main__':
     mode = 'gru'
     seqlen = 16
-    num_envs = None
     n_steps = 16 * Unit.K
     eval_freq = Unit.K
-    batch_size = Unit.K
-
+    batch_size = 256
     num_epochs = Unit.K
+    num_circuits = 5
 
     dataset = CircuitDataset('20Q_gate_Tokyo')
+    min_gatelen, max_gatelen = dataset.cx_num_range
+    step_size = 100
 
-    for path in dataset.sample(num_circuits=10, min_gatelen=1, max_gatelen=100):
-        run_maskable_ppo(
-            hardware='Tokyo',
-            circuit_path=path,
-            seqlen=seqlen,
-            final_reward='cx_num',
-            output_dirname=f'tmp',
-            reward_shaping_weight=10,
-            mode=mode,
-            save_result=True,
-            skip_existing=True,
-            num_epochs=num_epochs,
-            batch_size=batch_size,
-            n_steps=n_steps,
-            eval_freq=eval_freq,
-            stop_if_no_improvement=True,
-        )
+    for gatelen in range(0, 600, 100):
+        print(gatelen, gatelen+step_size)
+        for path in dataset.sample(num_circuits=num_circuits, min_gatelen=gatelen, max_gatelen=gatelen + step_size):
+            print(path)
+
+            run_maskable_ppo(
+                hardware='Tokyo',
+                circuit_path=path,
+                seqlen=seqlen,
+                final_reward='cx_num',
+                output_dirname=f'20Q_gate_Tokyo_num_circuits={num_circuits}_sample',
+                reward_shaping_weight=10,
+                mode=mode,
+                save_result=True,
+                skip_existing=True,
+                num_epochs=num_epochs,
+                batch_size=batch_size,
+                n_steps=n_steps,
+                eval_freq=eval_freq,
+                stop_if_no_improvement=True,
+            )
