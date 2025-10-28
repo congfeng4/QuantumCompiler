6ca7717f534a79864653a50ffcfe3776b7138354
huawei-od-man <2120400017@stu.hrbust.edu.cn>
2025-10-28 13:45:18 +0800

    验证电路等价性，批量，WIP
    qiskit有两个电路transpile之后不等价。

--- contrib/verify_circuit.py
+++ contrib/verify_circuit.py
@@ -1,20 +1,28 @@
+from pathlib import Path
 from typing import Union
 
 import networkx as nx
 import numpy as np
-from qiskit import QuantumCircuit, QuantumRegister
+from qiskit import QuantumCircuit, QuantumRegister, transpile
 from qiskit.circuit import Instruction, CircuitInstruction
 from qiskit.quantum_info import Statevector, Operator
 from qiskit.transpiler import CouplingMap, Layout, PassManager
 from qiskit.circuit.library import Permutation
 from qiskit.transpiler.passes import ApplyLayout, SetLayout
+from qiskit import transpile
 
-from contrib.common import GATE_NAME_MAPPING, get_inverse_mapping, convert_to_int_mapping
+from contrib.common import GATE_NAME_MAPPING, get_inverse_mapping, convert_to_int_mapping, read_mapping, read_json
+from contrib.random_graphs import closest_factors
+from qiskit.quantum_info.random import random_pauli
+from qiskit.primitives.backend_estimator_v2 import BackendEstimatorV2 as Estimator
+
+
 
 THRESHOLD = 1e-10
 
 
 def check_equivalence_under_mapping(qc_in, qc_out, initial_mapping):
+    # 对于qiskit transpile出来的电路，不需要Apply Layout。
     if isinstance(initial_mapping, Layout):
         initial_layout = initial_mapping
     else:
@@ -27,7 +35,6 @@ def check_equivalence_under_mapping(qc_in, qc_out, initial_mapping):
 
 
 def verify_circuit_routed(qc: QuantumCircuit, graph: Union[CouplingMap, nx.Graph], initial_mapping):
-
     inverse_mapping = get_inverse_mapping(initial_mapping)
 
     def has_edge(u, v):
@@ -37,7 +44,7 @@ def verify_circuit_routed(qc: QuantumCircuit, graph: Union[CouplingMap, nx.Graph
             return graph.has_edge(u, v)
         raise TypeError(graph)
 
-    for inst in qc.data: # type: CircuitInstruction
+    for inst in qc.data:  # type: CircuitInstruction
         qubits = inst.qubits
         if len(qubits) == 1:
             continue
@@ -55,5 +62,54 @@ def verify_circuit_routed(qc: QuantumCircuit, graph: Union[CouplingMap, nx.Graph
     return True
 
 
+def check_output_ours(ours_dir: Path, qubits_threshold=10):
+    for subdir in Path(ours_dir).iterdir():
+        qc_in = QuantumCircuit.from_qasm_file(subdir / 'input_circuit.qasm')
+        qc_out = QuantumCircuit.from_qasm_file(subdir / 'output_circuit.qasm')
+        initial_mapping = read_mapping(subdir / 'initial_mapping.json')
+        # final_mapping = read_mapping(subdir / 'final_mapping.json')
+        if qc_in.num_qubits > qubits_threshold:
+            print('skip', subdir.name, qc_in.num_qubits)
+            continue
+        ok = check_equivalence_under_mapping(qc_in, qc_out, initial_mapping)
+        # print(subdir.name, ok)
+        assert ok
+
+
+def check_qiskit_transpile(data_dir: Path, qubits_threshold=10):
+    ok_basic = set()
+    notok_basic = set()
+
+    for qc_path in Path(data_dir).glob('*.qasm'):
+        qc_in = QuantumCircuit.from_qasm_file(qc_path)
+        num_qubits = qc_in.num_qubits
+        if num_qubits > qubits_threshold:
+            # print('skip', qc_path.name, num_qubits)
+            continue
+
+        rows, cols = closest_factors(num_qubits)
+
+        for name, cm in [('line', CouplingMap.from_line(num_qubits)), ('grid', CouplingMap.from_grid(rows, cols)),
+                   ('ring', CouplingMap.from_ring(num_qubits)) ]:
+            for routing_method in ['sabre', 'basic']:
+                for layout_method in ['trivial', 'dense', 'sabre']:
+                    for opt_level in range(4):
+                        qc_out = transpile(qc_in, coupling_map=cm,
+                                           layout_method=layout_method, routing_method=routing_method,
+                                           optimization_level=opt_level)
+                        assert qc_out.num_qubits == qc_in.num_qubits
+
+                        ok = check_equivalence_under_mapping(qc_in, qc_out, qc_out.layout.initial_layout)
+                        if not ok:
+                            print(qc_in.global_phase, qc_out.global_phase)
+                            notok_basic.update(qc_in.count_ops().keys())
+                            print(qc_path.name, routing_method, layout_method, opt_level, name)
+                        else:
+                            ok_basic.update(qc_in.count_ops().keys())
+
+    print(ok_basic, notok_basic)
+
+
 if __name__ == '__main__':
-    pass
+    check_qiskit_transpile(Path('../data/nam_circs'))
+    # check_output_ours('../output/ours')
